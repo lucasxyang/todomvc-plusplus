f322f3033dc59ee27aba3aea86f94c9b39c35ff3
yang4131 <yang4131@umn.edu>
2016-08-27 12:37:53 -0500

    merged master against origin/master

--- src/server/webapp.js
+++ src/server/webapp.js
@@ -4,18 +4,35 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./controllers/routes');
-const morgan = require('morgan')
+//<<<<<<< Updated upstream
+//const morgan = require('morgan')
+//=======
+const morgan = require('morgan');
+//>>>>>>> Stashed changes
 
 let app = express();
 
 // Configure view engine and views directory
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.set('x-powered-by', false);
+//<<<<<<< Updated upstream
+//app.set('x-powered-by', false);
+//=======
+app.disable('x-powered-by');
+//>>>>>>> Stashed changes
 
 // Configure middleware
 app.use(morgan('combined'))
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('combined'));
+
+
+// Add the "No Shenanigans" header to all responses under the "/todos" path
+app.use('/todos', (request, response, next) => {
+  response.set('X-Shenanigans', 'None');
+  next();
+});
+
 
 // Static file serving happens everywhere but in production
 if (process.env.NODE_ENV !== 'production') {
